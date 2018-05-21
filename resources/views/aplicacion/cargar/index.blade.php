c1e1972b6409323dff103f8c6a67ab443b468546
Héctor Alfredo Chiguay Copa <hector2c@live.com>
2018-05-21 09:21:38 -0500

    Se modifica la plantilla aplicacion

--- resources/views/aplicacion/cargar/index.blade.php
+++ resources/views/aplicacion/cargar/index.blade.php
@@ -3,12 +3,6 @@
 
 @section('content')
 
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item active" aria-current="page">Cargar</li>
-    </ol>
-</nav>
-
 <table class="table table-bordered table-hover table-condensed">
     <thead>
         <tr>
